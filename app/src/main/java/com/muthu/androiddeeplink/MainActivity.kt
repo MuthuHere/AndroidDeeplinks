f5b9bcfeb790b8f0fbf8778d2fe8084894632670
Muthu <s.muthukhumar@gmail.com>
2019-06-25 11:31:00 +0800

    Initial commit

---
+++ app/src/main/java/com/muthu/androiddeeplink/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.muthu.androiddeeplink
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+
+        val data = intent?.data
+
+        if(data!=null){
+            val pathSegments = data.pathSegments
+            if(pathSegments.isNotEmpty()){
+                tvData.text = data.pathSegments[0]
+            }
+
+        }
+
+
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
